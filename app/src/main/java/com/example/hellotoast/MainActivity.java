8b3321bbf9a0f4e63670006a9bc071ca72574d75
= <=>
2022-09-22 21:21:07 +0700

    update change color

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button mButtonZero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mButtonZero = (Button)findViewById(R.id.button_zero);
+        setColor(findViewById(R.id.button_count), mCount % 2 == 0 ? Color.RED : R.color.green);
     }
 
     public void showToast(View view) {
@@ -26,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null) {
+            mButtonZero.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            if(mCount%2 == 0){
+                view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            }
+            else{
+                view.setBackgroundColor(getResources().getColor(R.color.green));
+            }
             mShowCount.setText(Integer.toString(mCount));
+
+            setColor(view, mCount % 2 == 0 ? Color.RED : R.color.green);
+            setColor(findViewById(R.id.button_zero), Color.BLACK);
         }
     }
 
     public void countZero(View view) {
         mCount = 0;
         mShowCount.setText("0");
+
+        setColor(view, Color.parseColor("#ffaaaaaa"));
+        setColor(findViewById(R.id.button_count), Color.RED);
+    }
+    private void setColor(View view, int color)
+    {
+        view.setBackgroundColor(color);
     }
 }
